bb96b566d3dcfc1fc1d7a3e12c0323af91c6af13
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2022-08-28 13:39:43 +0530

    Draw a rectangle

--- example.c
+++ example.c
@@ -7,6 +7,24 @@
 #define WIDTH 800
 #define HEIGHT 600
 
+static uint32_t pixels[HEIGHT*WIDTH];
+
+void olivec_fill_rect(uint32_t *pixels, size_t pixels_width, size_t pixels_height, 
+                      int x0, int y0, size_t w, size_t h,
+                      uint32_t color) 
+{
+  for(int dy = 0; dy < (int)h; ++dy) {
+    int y = y0 + dy;
+    for(int dx = 0; dx < (int) w; ++dx) {
+      int x = x0 + dx;
+      if (0 <= x && x < (int)pixels_width) {
+        if (0 <= y && y < (int)pixels_height) {
+          pixels[y * pixels_width + x] = color;
+        }
+      }
+    }
+  }
+}
 
 void olivec_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color)
 {
@@ -53,11 +71,11 @@ defer:
   return result;
 }
 
-static uint32_t pixels[HEIGHT*WIDTH];
 
 int main(void)
 {
-  olivec_fill(pixels, WIDTH, HEIGHT, 0xFF0000FF);
+  olivec_fill(pixels, WIDTH, HEIGHT, 0xFF202020);
+  olivec_fill_rect(pixels, WIDTH, HEIGHT, 0, 0, 50, 30, 0XFF2020FF);
   const char *file_path = "output.ppm";
   Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
   if (err) {
