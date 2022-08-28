7d20ba03f933e32baf2f089d30ba1b170dba3d0b
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2022-08-28 13:24:59 +0530

    PPM render fix

--- example.c
+++ example.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <errno.h>
 #include "olive.c"
 
 #define WIDTH 800
 #define HEIGHT 600
 
-static uint32_t pixels[HEIGHT][WIDTH];
 
 void olivec_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color)
 {
@@ -14,22 +14,37 @@ void olivec_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color)
   }
 }
 
+typedef int Errno;
 #define return_defer(value) do { result = (value); goto defer; } while (0)
 
 int olivec_save_to_ppm_file(uint32_t *pixels, size_t width, size_t height, const char *file_path)
 {
   int result = 0;
-  FILE *f = fopen(file_path, "wb");
+  FILE *f = NULL;
+  
+  {
+    f = fopen(file_path, "wb");
 
-  // printf("%lu", (unsigned long)pixels);
+    if (f == NULL) {
+      return_defer(-1);
+    }
 
-  if (f == NULL) {
-    return_defer(-1);
-  }
+    fprintf(f, "P6\n%zu %zu 255\n", width, height);
+    if (ferror(f))  return_defer(-1);
 
-  fprintf(f, "P6\n%zu %zu 255\n", width, height);
-  if (ferror(f))  {
-    return_defer(-1);
+    for(size_t i = 0; i < width*height; ++i) {
+      // 0xAABBGGRR 
+      uint32_t pixel = pixels[i];
+
+      uint8_t bytes[3] = {
+        (pixel>>(8*0))&0xFF,
+        (pixel>>(8*1))&0xFF,
+        (pixel>>(8*2))&0xFF,
+      };
+      fwrite(bytes, sizeof(bytes), 1, f);
+      if (ferror(f))  return_defer(errno);
+
+    }
   }
 
 defer:
@@ -37,9 +52,10 @@ defer:
   return result;
 }
 
+static uint32_t pixels[HEIGHT*WIDTH];
+
 int main(void)
 {
-  // printf("Hello, World\n");
   olivec_fill(pixels, WIDTH, HEIGHT, 0xFF0000FF);
   olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, "output.ppm");
 
