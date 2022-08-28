3d5020f9f0a4898c84ab54224c23f8d4b2352915
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2022-08-28 13:29:36 +0530

    Error handling

--- example.c
+++ example.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <errno.h>
+#include <string.h>
 #include "olive.c"
 
 #define WIDTH 800
@@ -57,8 +58,12 @@ static uint32_t pixels[HEIGHT*WIDTH];
 int main(void)
 {
   olivec_fill(pixels, WIDTH, HEIGHT, 0xFF0000FF);
-  olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, "output.ppm");
-
+  const char *file_path = "output.ppm";
+  Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
+  if (err) {
+    fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
+    return 1;
+  }
   return 0;
 }
 
